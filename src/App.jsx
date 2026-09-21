eab019184dcf28588f374bb2bba1b409453679c4
Sebastian Åkerman <akermans222@gmail.com>
2026-09-21 13:27:54 +0200

    Initial React portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <h1>Sebastian Åkerman</h1>
+        <p>Full-Stack Developer</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header>
+      <div className="logo">Sebastian Åkerman</div>
+
+      <nav>
+        <a href="#about">About</a>
+        <a href="#skills">Skills</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
